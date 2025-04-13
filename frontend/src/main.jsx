750cfc47b90f9290853c79c762707134cb16a723
shashi516 <149226232+shashi516@users.noreply.github.com>
2025-04-13 23:57:49 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 
 let persistor = persistStore(store)
-
+export const BASE_URL="http://localhost:5001"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
